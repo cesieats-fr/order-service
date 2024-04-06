fa6db61e1ed6e65a88df0b723d0abe8de2e6c61f
Mayonni <mathieu.maion0@gmail.com>
2024-04-06 14:44:15 +0200

    fix add order, get order

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { IOrder, IOrderItems } from 'cesieats-service-types/order';
 import { Order, OrderItems } from '../database';
 
-const addOrder = (req: Request, res: Response) => {
+const addOrder = async (req: Request, res: Response) => {
   const order: IOrder = {
     id: '0',
     idClient: '0',
@@ -10,17 +10,19 @@ const addOrder = (req: Request, res: Response) => {
     itemState: 0,
     idDelivery: '0',
   };
-  const result = Order.create(order);
+  const result = await Order.create(order);
+  console.log(result);
   res.status(200).json(result);
 };
 
 //  
-const changeOrderState = (req: Request, res: Response) => {
+const changeOrderState = async (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello, world!' });
 };
 
-const getOrder = (req: Request, res: Response) => {
-  const result = Order.findById(req.query.id);
+const getOrder = async (req: Request, res: Response) => {
+  const result = await Order.findById(req.query.id);
+  console.log(result);
   res.status(200).json(result);
 };
 
