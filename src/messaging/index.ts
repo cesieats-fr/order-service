9e806d861f51a24cca82c51b561f20018ea642e6
Keanu <keanuharrell@hotmail.fr>
2024-04-07 16:48:21 +0200

    change credentials to var env

--- src/messaging/index.ts
+++ src/messaging/index.ts
@@ -6,17 +6,20 @@ interface IMessaging {
   connection: amqp.Connection;
   senders: Senders;
   receivers: Receivers;
+  channel: amqp.Channel;
 }
 
 class Messaging implements IMessaging {
   connection: amqp.Connection;
   senders: Senders;
   receivers: Receivers;
+  channel: amqp.Channel;
 
-  constructor(connection: amqp.Connection, senders: Senders, receivers: Receivers) {
+  constructor(connection: amqp.Connection, channel: amqp.Channel, senders: Senders, receivers: Receivers) {
     this.connection = connection;
     this.senders = senders;
     this.receivers = receivers;
+    this.channel = channel;
   }
 
   disconnect() {
@@ -27,16 +30,15 @@ class Messaging implements IMessaging {
 }
 
 export async function connectRabbitMQ() {
-  const connection = await amqp.connect(`amqp://${process.env.RABBITMQ_URL}:5672`, {
-    username: 'guest',
-    password: 'guest',
+  const connection = await amqp.connect(`amqp://${process.env.RABBITMQ_URL}`, {
+    username: process.env.RABBITMQ_USERNAME,
+    password: process.env.RABBITMQ_PASSWORD,
   });
   console.log('Connected to RabbitMQ ');
-  const receiveChannel = await connection.createChannel();
-  const sendChannel = await connection.createChannel();
-  const receivers = new Receivers(receiveChannel);
-  const senders = new Senders(sendChannel);
-  return new Messaging(connection, senders, receivers);
+  const channel = await connection.createChannel();
+  const receivers = new Receivers(channel);
+  const senders = new Senders(channel);
+  return new Messaging(connection, channel, senders, receivers);
 }
 
 export default Messaging;
